d60b626a553b62a337c18176824339fa97842b01
adebert <debert@gmail.com>
2024-09-22 20:14:25 -0300

    Addeed more tests!

--- src/pino-log-link/addLogLink.test.ts
+++ src/pino-log-link/addLogLink.test.ts
@@ -23,7 +23,50 @@ describe('addLogLinkToFormatter', () => {
             message: 'hello http://localhost:3000/project/src/moduleA.ts'
         });
     });
+    it('should add a log link to the log message when no formatter is defined', () => {
+        const moduleMap = {
+            'moduleA': '/project/src/moduleA.ts',
+        };
+        const formatters = {
+        };
+
+        // Call addLogLink to modify the formatters object
+        const modifiedFormatters = addLogLinkToFormatter(formatters, "log", 'moduleName', () => moduleMap, 'http://localhost:3000/', 'message');
+
+        const logRecord = { moduleName: 'moduleA', message: 'hello' };
+
+        // Directly call the modified formatter function
+        const result = modifiedFormatters.log(logRecord);
+
+        expect(result).toEqual({
+            message: 'hello http://localhost:3000/project/src/moduleA.ts'
+        });
+    });
+
+    it('should work when logRecord is missing moduleName', () => {
+        const moduleMap = {
+            'moduleA': '/project/src/moduleA.ts',
+        };
+        const formatters = {
+            log: function (logRecord: any) {
+                return logRecord;
+            },
+        };
+
+        // Call addLogLink to modify the formatters object
+        const modifiedFormatters = addLogLinkToFormatter(formatters, "log", 'moduleName', () => moduleMap, 'http://localhost:3000/', 'message');
+
+        const logRecord = { message: 'hello' };
+
+        // Directly call the modified formatter function
+        const result = modifiedFormatters.log(logRecord);
+
+        expect(result).toEqual({
+            message: 'hello not found!'
+        });
+    });
 });
+
 
 describe('addLogLink', () => {
     it('should add a log link to the log message', () => {
